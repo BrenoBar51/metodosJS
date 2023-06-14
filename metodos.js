5577674d182ca9d3b13caa3dde7ebc277ac799fb
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-06-14 08:35:06 -0300

    initial commit

---
+++ metodos.js
@@ -0,0 +1,18 @@
+const container = document.getElementById9('container')
+
+const clientes = [
+    {
+        nome: "Breno",
+        email: "brenobarcelos9@gmail.com",
+        cidade: "Fortaleza",
+        estado:"CE",
+        idade:18
+    },
+    {
+        nome: "Breno",
+        email: "brenobarcelos9@gmail.com",
+        cidade: "Fortaleza",
+        estado:"CE",
+        idade:18
+    }
+]
