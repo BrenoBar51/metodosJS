08202cbd3ade6dca7f7debd38108c1f0f0734b7a
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-06-14 09:16:35 -0300

    reduce

--- metodos.js
+++ metodos.js
@@ -1,4 +1,5 @@
-const container = document.getElementById9('container')
+/*Map
+const container = document.getElementById('container')
 
 const clientes = [
     {
@@ -16,3 +17,29 @@ const clientes = [
         idade:18
     }
 ]
+clientes.map((cliente, i) => {
+    console.log(cliente, i)
+})
+clientes.map((cliente, i) => {
+    container.innerHTML += `
+        <div>
+            <p>Posição no Array: ${i}</p>
+            <p>${cliente.nome}</p>
+            <p>${cliente.email}</p>
+            <p>${cliente.cidade}</p>
+            <p>${cliente.estado}</p>
+            <p>${cliente.idade}</p>
+        </div>
+        <hr>
+    `
+})*/
+/*Reduce*/
+const container = document.getElementById('container')
+const vendas = [50, 120, 100, 20, 15]
+const somaVendas = vendas.reduce(
+        (valorAnterior, valorAtual) => {
+            let total = valorAnterior + valorAtual
+            return total
+        }, 0
+    );
+console.log(somaVendas)
